81c6aeeab7355fd9b359c4d2bc4278da96bb863b
unknown <alejandro7899871776@gmail.com>
2023-08-29 09:17:52 -0500

    memory deploitment

--- app.py
+++ app.py
@@ -4,19 +4,29 @@ import os
 from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate, OpenAI, LLMChain
 from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import OpenAI
 
 
 os.environ["OPENAI_API_KEY"] = ""
 
-template = """Question: {question}
+template = """You are a nice chatbot having a conversation with a human, asnwers the question step by step.
 
-Answer: Let's think step by step."""
+Previous conversation:
+{chat_history}
+
+New human question: {question}
+Response:"""
 
 @cl.on_chat_start
 def main():
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm= ChatOpenAI(model_name="gpt-4", temperature=0,verbose=True))
+    prompt = PromptTemplate.from_template(template)
+
+    memory = ConversationBufferMemory(memory_key="chat_history")
+
+    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(model_name="gpt-4", temperature=0,verbose=True),memory=memory,verbose=True)
     cl.user_session.set("llm_chain", llm_chain)
+
 
 
 @cl.on_message
